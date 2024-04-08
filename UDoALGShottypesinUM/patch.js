1b62f3d0d5f120ab47034751007cc63db1de0130
Neo Nickz <lkiutfr98276543@gmail.com>
2024-04-08 05:34:57 -0400

    lol2

--- UDoALGShottypesinUM/patch.js
+++ UDoALGShottypesinUM/patch.js
@@ -6,5 +6,5 @@
 	"servers": [
 		"https://mirrors.thpatch.net/neonickz/UDoALGShottypesinUM"
 	],
-	"title": "(TH18) Ports Zanmu, Orin, Ran and Enoko's shottypes from UDoALG into UM./n Please check the ingame manual for more information about this mod!/n Credit and thank you to Khangaroo for helping me with Sakuya/Ran's shottype."
+	"title": "(TH18) Ports Zanmu, Orin, Ran and Enoko's shottypes from UDoALG into UM.\n Please check the ingame manual for more information about this mod!\n Credit and thank you to Khangaroo for helping me with Sakuya/Ran's shottype."
 }
