710d2dd18c3a71b222cdac8076437f595a4fc567
Neo Nickz <lkiutfr98276543@gmail.com>
2024-04-08 00:10:15 -0400

    an

--- repo.js
+++ repo.js
@@ -1,13 +1,12 @@
 {
-    "id": "NeoNickz",
-    "title": "Neo Nickz's Shottype Mods",
-    "contact": "@NeoNickz",
-    "servers": [
-        "https://mirrors.thpatch.net/neonickz"
-    ],
-    "patches": {
-        "th15aya": "(TH15) Replaces Reisen with HSiFS Aya. Bomb is unchanged."
-		"UDoALGShottypesinUM": "(TH18) Ports Zanmu, Orin, Ran and Enoko's shottypes from UDoALG into UM./n Please check the ingame manual for more information about this mod!/n Credit and thank you to Khangaroo for helping me with Sakuya/Ran's shottype."
-    
-	}
+	"contact": "@neonickz",
+	"id": "neonickz",
+	"patches": {
+		 "th15aya": "(TH15) Replaces Reisen with HSiFS Aya. Bomb is unchanged.",
+		 "UDoALGShottypesinUM": "(TH18) Ports Zanmu, Orin, Ran and Enoko's Shottypes from UDoALG into UM./n Credit and thank you to Khangaroo for helping me with Sakuya/Ran./n Check the ingame manual for more information about this mod!"
+	},
+	"servers": [
+		"https://mirrors.thpatch.net/neonickz/"
+	],
+	"title": "Neo Nickzs Shottype Mods"
 }
