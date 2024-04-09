ac8a6d2c4a2c4adaa561b9fe53744b340828548f
Neo Nickz <lkiutfr98276543@gmail.com>
2024-04-08 22:59:59 -0400

    update, aya over sanae now

--- th15aya/patch.js
+++ th15aya/patch.js
@@ -6,5 +6,5 @@
         "servers": [
         "https://mirrors.thpatch.net/neonickz/th15aya"
         ],
-        "title": "(TH15) Replaces Reisen with HSiFS Aya. Bomb is unchanged."
+        "title": "(TH15) Replaces Sanae with HSiFS Aya. Bomb is unchanged."
 }
