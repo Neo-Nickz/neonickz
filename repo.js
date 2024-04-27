375ad77d308eee2ad62a162caa33fc2e211f160a
Neo Nickz <lkiutfr98276543@gmail.com>
2024-04-27 01:38:53 -0400

    again.

--- repo.js
+++ repo.js
@@ -4,7 +4,7 @@
 	"patches": {
 		 "TH15Aya": "(TH15) Replaces Sanae with HSiFS Aya.",
 		 "UDoALGShottypesinUM": "(TH18) Recreates Zanmu, Orin, Ran and Enoko's Shottypes from UDoALG into UM.\n Credit and thank you to Khangaroo for helping me with Sakuya/Ran.\n Check the ingame manual for more information about this mod!",
-	     "PCBReimuInUFO": "(TH12) Recreates PCB Reimu A and B in UFO. As per usual with my mods, bombs are unchanged.", 
+	     "PCBReimuInUFO": "(TH12) Recreates PCB Reimu A and B in UFO. As per usual with my mods, bombs are unchanged."
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/neonickz/"
