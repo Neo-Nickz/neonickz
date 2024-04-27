a478961fb32ba054428d3e6f7f0521a2bd3f2641
Neo Nickz <lkiutfr98276543@gmail.com>
2024-04-27 01:30:47 -0400

    lol

--- repo.js
+++ repo.js
@@ -5,7 +5,6 @@
 		 "TH15Aya": "(TH15) Replaces Sanae with HSiFS Aya.",
 		 "UDoALGShottypesinUM": "(TH18) Recreates Zanmu, Orin, Ran and Enoko's Shottypes from UDoALG into UM.\n Credit and thank you to Khangaroo for helping me with Sakuya/Ran.\n Check the ingame manual for more information about this mod!",
 	     "PCBReimuinUFO": "(TH12) Recreates PCB Reimu A and B in UFO. As per usual with my mods, bombs are unchanged.", 
-		 "SoEWReimuCinHSiFS": "(TH16) Replaces Aya with SoEW ReimuC.\n To be as accurate as possible to SoEW, Reimu's focus and unfocus speeds are the same, and you move slow as shit...! You can still see your hitbox when focused though.\n A special thank you to TESM for allowing me to use his PC98 Reimu portrait sprites.\n HITBOX SHIFTING AND TAP FIRERATE NOT INCLUDED"
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/neonickz/"
