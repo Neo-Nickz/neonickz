1c0beac8feea286b9ff2611d7353007ccedeb164
Neo Nickz <lkiutfr98276543@gmail.com>
2024-04-27 12:53:14 -0400

    chAP

---
+++ SoEWReimuC/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+        "id": "SoEWReimuC",
+        "servers": [
+        "https://mirrors.thpatch.net/neonickz/SoEWReimuC"
+        ],
+        "title": "(TH16) Replaces Aya with SoEW ReimuC.\n To be as accurate as possible to SoEW, Reimu's focus and unfocus speeds are the same, and you're slow as shit!\n You can still see your hitbox when focused though.\n A special thank you to TESM for allowing me to use his PC98 Reimu portrait sprites from MiDI.\n HITBOX SHIFTING AND TAP FIRERATE NOT INCLUDED"
+}
+  
