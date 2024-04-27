f0d79b85d079667b1948f5740702f402955fe0ce
Neo Nickz <lkiutfr98276543@gmail.com>
2024-04-27 11:47:31 -0400

    new mod

--- repo.js
+++ repo.js
@@ -2,9 +2,10 @@
 	"contact": "@neonickz",
 	"id": "neonickz",
 	"patches": {
-		 "TH15Aya": "(TH15) Replaces Sanae with HSiFS Aya.",
-		 "UDoALGShottypesinUM": "(TH18) Recreates Zanmu, Orin, Ran and Enoko's Shottypes from UDoALG into UM.\n Credit and thank you to Khangaroo for helping me with Sakuya/Ran.\n Check the ingame manual for more information about this mod!",
-	     "PCBReimuInUFO": "(TH12) Recreates PCB Reimu A and B in UFO. As per usual with my mods, bombs are unchanged."
+		 "TH15Aya": "(TH15) Replaces Sanae with HSiFS Aya. Not in menus cuz i'm too lazy to replace those files.",
+		 "UDoALGShottypesinUM": "(TH18) Recreates Zanmu, Orin, Ran and Enoko's Shottypes from UDoALG into UM.\n Credit and thank you to Khangaroo for helping me with Sakuya/Ran.\n Check the ingame manual for more information about this mod!"
+	     "PCBReimuinUFO": "(TH12) Recreates PCB Reimu A and B in UFO. As per usual with my mods, bombs are unchanged." 
+		 "SoEWReimuCinHSiFS": "(TH16) Replaces Aya with SoEW ReimuC.\n To be as accurate as possible to SoEW, Reimu's focus and unfocus speeds are the same, and you're slow as shit! You can still see your hitbox when focused though.\n A special thank you to TESM for allowing me to use his PC98 Reimu portrait sprites from MiDI.\n HITBOX SHIFTING AND TAP FIRERATE NOT INCLUDED"
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/neonickz/"
