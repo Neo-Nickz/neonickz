5409469086b1cff7208783f1afa964d647b703c0
Neo Nickz <lkiutfr98276543@gmail.com>
2024-05-03 22:05:13 -0400

    hm

---
+++ EoSDMarisaUFO/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+        "id": "EoSDMarisaUFO",
+        "servers": [
+        "https://mirrors.thpatch.net/neonickz/EoSDMarisaUFO"
+        ],
+        "title": "(TH12) Recreates EoSD Marisa A and B in UFO. PS. When you Power up with MarisaA after Power 2, stop shooting for half a second to update the laser damage and sprite. (No clue why it works like this lol)"
+}
---
+++ PCBReimuUFO/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+        "id": "PCBReimuUFO",
+        "servers": [
+        "https://mirrors.thpatch.net/neonickz/PCBReimuUFO"
+        ],
+        "title": "(TH12) Recreates PCB Reimu A and B in UFO. As per usual with my mods, bombs are unchanged."
+}
