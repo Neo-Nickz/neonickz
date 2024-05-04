ab0d2a78660c9efc92a07eb32a0edf37e5b55775
Neo Nickz <lkiutfr98276543@gmail.com>
2024-05-03 20:25:00 -0400

    lol

--- repo.js
+++ repo.js
@@ -5,7 +5,7 @@
 		 "TH15Aya": "(TH15) Replaces Sanae with HSiFS Aya. Not in menus cuz i'm too lazy to replace those files.",
 		 "UDoALGShottypesUM": "(TH18) Recreates Zanmu, Orin, Ran and Enoko's Shottypes from UDoALG into UM.\n Credit and thank you to Khangaroo for helping me with Sakuya/Ran.\n Check the ingame manual for more information about this mod!",
 	     "PCBReimuinUFO": "(TH12) Recreates PCB Reimu A and B in UFO. As per usual with my mods, bombs are unchanged.", 
-		 "SoEWReimuC": "(TH16) Replaces Aya with SoEW ReimuC.\n To be as accurate as possible to SoEW, Reimu's focus and unfocus speeds are the same, and you're slow as shit!\n You can still see your hitbox when focused though.\n A special thank you to TESM for allowing me to use his PC98 Reimu portrait sprites from MiDI.\n HITBOX SHIFTING AND TAP FIRERATE NOT INCLUDED"
+		 "SoEWReimuC": "(TH16) Replaces Aya with SoEW ReimuC.\n To be as accurate as possible to SoEW, Reimu's focus and unfocus speeds are the same, and you're slow as shit!\n You can still see your hitbox when focused though.\n A special thank you to TESM for allowing me to use his PC98 Reimu portrait sprites from MiDI.\n HITBOX SHIFTING AND TAP FIRERATE NOT INCLUDED",
 	     "DoremyNon1Fix": "title": "(TH15) This patch fixes the mess that is Doremy's non 1. ECL change by okaylol, i'm just hosting it.\n Note that this patch only targets the Lunatic version of non 1."
 	"servers": [
 		"https://mirrors.thpatch.net/neonickz/"
