4cb818e1c4a75720f4fd7efa1cf6e789682834d9
Neo Nickz <lkiutfr98276543@gmail.com>
2024-05-03 22:18:13 -0400

    s

--- repo.js
+++ repo.js
@@ -10,8 +10,6 @@
 		 "UDoALGShottypesUM": "(TH18) Recreates Zanmu, Orin, Ran and Enoko's Shottypes from UDoALG into UM.\n Credit and thank you to Khangaroo for helping me with Sakuya/Ran.\n Check the ingame manual for more information about this mod!",
 	     "PCBReimuUFO": "(TH12) Recreates PCB Reimu A and B in UFO. As per usual with my mods, bombs are unchanged.", 
 		 "SoEWReimuC": "(TH16) Replaces Aya with SoEW ReimuC.\n To be as accurate as possible to SoEW, Reimu's focus and unfocus speeds are the same, and you're slow as shit!\n You can still see your hitbox when focused though.\n A special thank you to TESM for allowing me to use his PC98 Reimu portrait sprites from MiDI.\n HITBOX SHIFTING AND TAP FIRERATE NOT INCLUDED",
-	     "DoremyNon1Fix": "(TH15) This patch fixes the mess that is Doremy's non 1. ECL change by okaylol, i'm just hosting it.\n Note that this patch only targets the Lunatic version of non 1.",
-         "EoSDMarisaUFO": "(TH12) Recreates EoSD Marisa A and B in UFO. PS. When you Power up with MarisaA after Power 2, stop shooting for half a second to update the laser damage and sprite. (No clue why it works like this lol)"
-
+	     "EoSDMarisaUFO": "(TH12) Recreates EoSD Marisa A and B in UFO. PS. When you Power up with MarisaA after Power 2, stop shooting for half a second to update the laser damage and sprite. (No clue why it works like this lol)"
     }
 }
