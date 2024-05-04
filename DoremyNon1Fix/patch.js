e1ca9e549403e69acbf6cc801f4dac6107dfb955
Neo Nickz <lkiutfr98276543@gmail.com>
2024-05-03 20:21:43 -0400

    okaylol patch

---
+++ DoremyNon1Fix/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+        "id": "DoremyNon1Fix",
+        "servers": [
+        "https://mirrors.thpatch.net/neonickz/DoremyNon1Fix"
+        ],
+        "title": "(TH15) This patch fixes the mess that is Doremy's non 1. ECL change by okaylol, i'm just hosting it."
+}
