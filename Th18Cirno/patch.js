38ee81922906ff1d116e4ce1cad40fcecc037fc1
Neo Nickz <lkiutfr98276543@gmail.com>
2024-05-17 20:57:45 -0400

    new mod

---
+++ Th18Cirno/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+        "id": "TH18Cirno",
+        "servers": [
+        "https://mirrors.thpatch.net/neonickz/TH18Cirno"
+        ],
+        "title": "(TH18) Replaces Sanae with HSiFS Cirno, damage cap included!\n No Releases Included."
+}
