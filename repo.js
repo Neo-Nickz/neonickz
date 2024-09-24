dcaf686ee286ef8a250331edcf44931bc286e4f8
Neo Nickz <lkiutfr98276543@gmail.com>
2024-09-23 23:50:03 -0400

    another lesanaeupdate

--- repo.js
+++ repo.js
@@ -6,8 +6,8 @@
 	    "https://mirrors.thpatch.net/neonickz/"
     ],
 	"patches": {
-		"TH15Aya": "(TH15) Replaces Sanae with HSiFS Aya. Not in menus cuz i'm too lazy to replace her portraits.",
-	    "PCBReimuUFO": "(TH12) Recreates PCB ReimuA and ReimuB in UFO. As per usual with my mods, bombs are unchanged.", 
+		"TH15Aya": "(TH15) Replaces Sanae with HSiFS Aya. Not in menus cuz i'm too lazy to replace her portraits. *update coming soon-ish*",
+	    "PCBReimuUFO": "(TH12) Recreates PCB ReimuA and ReimuB in UFO. As per usual with my mods, bombs are unchanged. *update coming soon-ish*", 
 		"SoEWReimuC": "(TH16) Replaces Aya with SoEW ReimuC.\n To be as accurate as possible to SoEW, Reimu's focus and unfocus speeds are the same, and you're slow as shit!\n You can still see your hitbox when focused though.\n A special thank you to TESM for allowing me to use his PC98 Reimu portrait sprites from MiDI.",
 	    "DoremyNon1Fix": "(TH15) This patch fixes the mess that is Doremy's non 1. ECL changes by okaylol, i'm just hosting it.\n Note that this patch only targets the Lunatic version of non 1.",
 		"Th15SakuyaB": "(TH15) Attempts to recreate DDC SakuyaB in LoLK, with the damage you know and love!\n Good luck LNNing with this. She replaces Reisen.",
