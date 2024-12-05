09c506c8234ba00fcf63df1c3de951b695afcfcd
neonickz <lkiutfr98276543@gmail.com>
2024-12-05 11:03:54 -0400

    mod

---
+++ YoukaiBuster/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+        "id": "YoukaiBuster",
+        "servers": [
+        "https://mirrors.thpatch.net/neonickz/YoukaiBuster"
+        ],
+        "title": "(TH13) Recreates MoF's ReimuC shottype into other games, usually replaces Reimu's shottype with Youkai Buster."
+}
