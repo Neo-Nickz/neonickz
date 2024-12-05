3ed444e76032cdc7bb1de644be40c6ae4952b4a7
neonickz <lkiutfr98276543@gmail.com>
2024-12-05 11:02:17 -0400

    newmodtest

--- repo.js
+++ repo.js
@@ -11,6 +11,7 @@
 		"SoEWReimuC": "(TH16) Replaces Aya with SoEW ReimuC.\n To be as accurate as possible to SoEW, Reimu's focus and unfocus speeds are the same, and you're slow as shit!\n You can still see your hitbox when focused though.\n A special thank you to TESM for allowing me to use his PC98 Reimu portrait sprites from MiDI.",
 	    "DoremyNon1Fix": "(TH15) This patch fixes the mess that is Doremy's non 1. ECL changes by okaylol, i'm just hosting it.\n Note that this patch only targets the Lunatic version of non 1.",
 		"Th15SakuyaB": "(TH15) Attempts to recreate DDC SakuyaB in LoLK, with the damage you know and love!\n Good luck LNNing with this. She replaces Reisen.",
-		"TH18Cirno": "(TH18) Replaces Sanae with HSiFS Cirno, damage cap included!\n No Releases Included."
+		"TH18Cirno": "(TH18) Replaces Sanae with HSiFS Cirno, damage cap included!\n No Releases Included.",
+		"YoukaiBuster": "(TH13) Recreates MoF's ReimuC shottype into other games, usually replaces Reimu's shottype with Youkai Buster."
     }
 }
