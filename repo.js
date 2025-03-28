a4c44a9836fcd644424ef4e0a22e764fc571dfbb
neonickz <lkiutfr98276543@gmail.com>
2025-03-27 23:49:32 -0400

    im still an idiot LMAO

--- repo.js
+++ repo.js
@@ -5,7 +5,7 @@
 		"DoremyNon1Fix": "(TH15) This patch fixes the second wave of Doremy's Non1. ECL changes by okaylol, i'm just hosting it.\n Note that this patch only targets the Lunatic version of non 1.",
 		"DrunktainOfFaith": "(TH10) MoF but it's trippy",
 		"PCBReimuUFO": "(TH12) Recreates PCB ReimuA and ReimuB in UFO. As per usual with my mods, bombs are unchanged.",
-		"QuickECLReload": "(TH15) This patch fixes the second wave of Doremy's Non1. ECL changes by okaylol, i'm just hosting it.\n Note that this patch only targets the Lunatic version of non 1.",
+		"QuickECLReload": "(TH10-TH18.5) Makes ESC + R reload ECLs \n Doesn't Support: TH06-TH09.5, TH12.8, TH12.5, TH19",
 		"SoEWReimuC": "(TH16) Replaces Aya with SoEW ReimuC.\n To be as accurate as possible to SoEW, Reimu's focus and unfocus speeds are the same, and you're slow as shit!\n You can still see your hitbox when focused though.\n A special thank you to TESM for allowing me to use his PC98 Reimu portrait sprites from MiDI.",
 		"Th15SakuyaB": "(TH15) Attempts to recreate DDC SakuyaB in LoLK, with damage... that's somehow better in this game than DDC? She replaces Reisen.",
 		"Th15Sukune": "(TH15) Recreates Len'en 3's Sukune in LoLK, comes with a custom bomb aswell. She replaces Sanae.\n Credit to RosePenguin100 for making the shottype and balancing it.\n also don't use th15aya with this lmao",
